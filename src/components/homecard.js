4e121259a7f026ea39de770fa339b36132ae1575
nju123 <211820266@smail.nju.edu.cn>
2024-02-08 21:41:37 +0800

    add homecard

---
+++ src/components/homecard.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button'; // 注意这里的导入方式
+
+const HomeCard = ({ color, headText, buttonText, buttonLink }) => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > :not(style)': {
+          m: 1,
+          width: 428,
+          height: 228,
+        },
+      }}
+    >
+      <Paper elevation={3} sx={{
+         p: 2,
+         backgroundColor: color, 
+         position: 'relative',// 设置相对定位
+         // position: 'relative' 设置在 Paper 组件上的作用是为内部绝对定位的 Box 组件提供一个定位上下文
+         // 父元素Relative 子元素absolute定位就是相对于父元素的
+         width: "100%", // 确保Paper填满Box容器
+         height: "100%",
+         }}> {/* 添加padding和背景色 */}
+        <h1 style={{ color: "white" }}>{headText}</h1> {/* 使用style属性而不是sx */}
+        <Box sx={{
+            position:'absolute',//绝对定位
+            bottom:16,
+            right:16,
+        }}>
+             <Button variant='contained' href={buttonLink}>{buttonText}</Button>
+        </Box>
+      </Paper>
+    </Box>
+  );
+}
+
+export default HomeCard;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,14 @@ import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
 import Seo from "../components/seo"
 import { Button } from '@mui/material'
+import HomeCard from '../components/homecard'
+
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
-      <Button variant='contained'>Hello World~</Button>
+      <Button variant='contained'>Hello World!</Button>
+      <HomeCard color="black" headText="GitHub" buttonText="去逛逛" buttonLink="https://github.com/nju123"></HomeCard>
       <StaticImage
         alt="ocean"
         src="https://images.unsplash.com/photo-1587502536575-6dfba0a6e017?w=1200&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxzZWFyY2h8MXx8c2VhfGVufDB8fDB8fHww"
