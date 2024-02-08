5557e0def212871f09eee80f2b1ac3f502b1c826
nju123 <211820266@smail.nju.edu.cn>
2024-02-08 20:02:02 +0800

    add a button

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,12 @@ import * as React from 'react'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
 import Seo from "../components/seo"
-
+import { Button } from '@mui/material'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <Button variant='contained'>Hello World~</Button>
       <StaticImage
         alt="ocean"
         src="https://images.unsplash.com/photo-1587502536575-6dfba0a6e017?w=1200&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxzZWFyY2h8MXx8c2VhfGVufDB8fDB8fHww"
