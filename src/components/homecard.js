e4e7f53f4c0e27c195c70e78b55460bf06ebd605
nju123 <211820266@smail.nju.edu.cn>
2024-02-13 13:39:37 +0800

    change the display style

--- src/components/homecard.js
+++ src/components/homecard.js
@@ -11,8 +11,8 @@ const HomeCard = ({ color, headText, buttonText, buttonLink }) => {
         flexWrap: 'wrap',
         '& > :not(style)': {
           m: 1,
-          width: 428,
-          height: 228,
+          width: 328,
+          height: 158,
         },
       }}
     >
@@ -38,4 +38,40 @@ const HomeCard = ({ color, headText, buttonText, buttonLink }) => {
   );
 }
 
-export default HomeCard;
+const HomeCardBig = ({ color, headText, buttonText, buttonLink,children }) => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > :not(style)': {
+          m: 1,
+          width: 328,
+          height: 158,
+        },
+      }}
+    >
+      <Paper elevation={3} sx={{
+         p: 2,
+         backgroundColor: color, 
+         position: 'relative',// 设置相对定位
+         // position: 'relative' 设置在 Paper 组件上的作用是为内部绝对定位的 Box 组件提供一个定位上下文
+         // 父元素Relative 子元素absolute定位就是相对于父元素的
+         width: "100%", // 确保Paper填满Box容器
+         height: "100%",
+         }}> {/* 添加padding和背景色 */}
+        <h1 style={{ color: "white" }}>{headText}</h1> {/* 使用style属性而不是sx */}
+        {children}
+        <Box sx={{
+            position:'absolute',//绝对定位
+            bottom:20,
+            right:20,
+        }}>
+             <Button variant='contained' href={buttonLink}>{buttonText}</Button>
+        </Box>
+      </Paper>
+    </Box>
+  );
+}
+
+export {HomeCard,HomeCardBig};
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
 import Seo from "../components/seo"
 import { Avatar, Box, Button } from '@mui/material'
-import HomeCard from '../components/homecard'
+import {HomeCard,HomeCardBig} from '../components/homecard'
 import { CenterFocusStrong } from '@mui/icons-material'
 
 const IndexPage = () => {
@@ -19,6 +19,7 @@ const IndexPage = () => {
 
 
         <Box sx={{
+          position:"fixed",
           width: "50%",
           height: "100%",
           alignItems: 'center',
@@ -44,10 +45,16 @@ const IndexPage = () => {
           width: "50%",
           height: "100%",
           margin: 'auto',
+          marginLeft:"50%", //因为左边的profile格子设置position为fixed，排版会乱，使用marginLeft来替代原来的效果
         }}>
+          <HomeCardBig color="#007FFF" headText="Timo" buttonText="了解更多">
+            <h3 style={{color:"white"}}>改变现状的Loser</h3>
+          </HomeCardBig>
           <HomeCard color="black" headText="GitHub" buttonText="去逛逛" buttonLink="https://github.com/nju123"></HomeCard>
           <HomeCard color="#FF6699" headText="B站" buttonText="去逛逛" buttonLink="https://space.bilibili.com/457257865?spm_id_from=333.1007.0.0"></HomeCard>
-          <HomeCard color="#007FFF" headText="博客" buttonText="去逛逛" buttonLink="\blog"></HomeCard>
+          <HomeCard color="#007FFF" headText="博客" buttonText="去逛逛" buttonLink="\blog">
+            
+          </HomeCard>
         </Box>
 
       </Box>
